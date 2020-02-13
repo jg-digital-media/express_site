931810385358584e4f827de820a73ca4904ecb5c
jg-digital-media <mail@jonniegrieve.co.uk>
2020-02-13 17:13:00 +0000

    update project S5 complete

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
 const express = require('express');
 
-//express function
+//import body-parser middleware
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+//Express launch function
 const app = express();
 
-//set up the development server listen method - port number
-app.listen(3000, function(){
-    console.log("server currently running on Heroku")
+//use bodyParser and cookieParser middleware - third party.
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
-});
+//set the view engine to use pug for templating
+app.set("view engine", "pug");
 
-///served the home route
-app.get('/', (req, res) => {
-    
-    //basic response with the send method
-     res.send('<h1>This is the home route</h1>');
-     res.end();
- });
+const indexRouter = require("./routes");
+const cardsRouter = require("./routes/cards");
 
- ///served the second route
- app.get('/second', (req, res) => {
-     
-    //basic response with the send method
-     res.send('<h1>This is the second route</h1>');
-     res.end();
+app.use(indexRouter);
+app.use('/cards', cardsRouter);
+
+/*MIDDLEWARE*/
+
+/* app.use((req, res, next) => {
+    console.log("Hello");
+    next();
  });
  
+ app.use((req, res, next) => {
+    console.log(", World");
+    next();
+ }); */
+
+//404 Middleware!
+app.use((req, res, next) => {
+    const err = new Error('Page Not Found!');
+    err.status = 404;
+    next(err);
+});
+
+//error handler middleware
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+    next();
+    //res.end();
+ 
+ });
+
+//Set up the development server listen method - port number
+app.listen(3000, () => {
+    console.log("server currently running on Heroku");
+
+});
---
+++ routes/cards.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const routes = express.Router();
+
+const { data } = require("../data/flashcardData.json");
+const { cards } = data;
+
+
+routes.get( '/', ( req, res ) => {
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor( Math.random() * numberOfCards );
+  res.redirect( `/cards/${flashcardId}?side=question` );
+});
+
+ //serve the cards route
+ routes.get('/:id', (req, res) => {
+   const { side } = req.query;
+   const { id } = req.params;
+
+
+   if ( !side ) {
+     res.redirect( `/cards/${id}?side=question` );
+   }
+
+   const name = req.cookies.username;
+   const text = cards[id][side];   
+   const { hint } = cards[id];
+
+   console.log(req.query);
+   console.log(req.query.side);
+   console.log(req.params);
+   console.log(cards[id][side]);
+   console.log(text);
+   console.log(cards[id]);
+
+   const templateData = { id, text: text, hint: hint, name};
+
+   if (side === 'question') {
+     templateData.hint = hint;
+     templateData.sideToShow = 'answer';
+     templateData.sideToShowDisplay = 'Answer';
+   } else if (side === 'answer') {
+     templateData.sideToShow = 'question';
+     templateData.sideToShowDisplay = 'Question';
+   }
+   
+   res.render('cards', templateData);
+});
+
+
+ //export cards routes to app.js
+ module.exports = routes;
