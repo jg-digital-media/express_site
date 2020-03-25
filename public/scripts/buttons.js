7517844d5e8bd0923d875a133aab2c4aa4ae7786
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-03-25 16:06:52 +0000

    add font, extends and colour variables

--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -1,14 +1,37 @@
-function buttonState(){
+/* function buttonState(){
     $("input").each(function(){
-        $('#name_btn').attr('disabled', 'disabled');
+        //$('#name_btn').attr('disabled', 'disabled');
         
         if($(this).val() != "" ) {
+            $('#name_btn').attr('disabled', 'disabled');
+        } else {
             $('#name_btn').attr('disabled', '');
+
         }
     })
 }
 
 $(function(){
-    $('#name_btn').attr('disabled', 'disabled');
-    $('input').change(buttonState);
+    //$('#name_btn').attr('disabled', 'disabled');
+    
+    $("input").change(buttonState);
 })
+ */
+
+/* $("#name_input").change(function(){   
+    $("#name_input").attr('disabled', !this.checked)
+}); */
+
+//toggle on basis of empty text
+$(document).ready(function() {
+    
+    $('#name_btn').prop('disabled', true);
+
+    $('#name_input').keyup(function() {
+       if($(this).val() != '') {
+          $('#name_btn').prop('disabled', false);
+       } else {
+        $('#name_btn').prop('disabled', true);
+       }
+    });
+});
